86a1d72237de1b5d3b72c1adb5a43c012b12fa13
MaximeD34 <max.dudognon@gmail.com>
2024-04-30 23:41:42 +0200

    test openai api

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,14 @@
 import os
 
 from flask import Flask
+from openai import OpenAI
+
 
 app = Flask(__name__)
+
+#setup the openai api key
+KEY = "sk-proj-WKoMoUxcBoLAooeZW7q6T3BlbkFJqKSe48dOzJzbEfOv4aag"
+client = OpenAI(api_key=KEY)
 
 @app.route('/')
 def hello():
@@ -11,6 +17,19 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return 'Hello {}! new version'.format(name)
+
+@app.route('/openai')
+def openai():
+    query = "What is the capital of France?"
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant, answer the following question."},
+            {"role": "user", "content": query}
+        ]
+    )
+    return completion.choices[0].message.content
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
